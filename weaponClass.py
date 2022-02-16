f2458b8d841eafcea07ba3bd85ec8a16bafaf6a8
Jiaxi Hu <Jiaxi_Hu1@baylor.edu>
2022-02-16 13:55:08 -0600

    comm

--- weaponClass.py
+++ weaponClass.py
@@ -22,6 +22,50 @@ every time the method is called. When the bullet count reaches zero, it should c
 the attribute 'status' to 'Inactive'
 
 '''
+class Weapon:
+    def __init__(self,name,b,speed,r,sta):
+        self.__name = name 
+        self.__bullets = b
+        self.__speed = speed 
+        self.__range = r 
+        self.__status = sta
+    
+    def accessor_name(self):
+        self.__name
+    
+    def accessor_bullets(self):
+        self.__bullets
+    
+    def accessor_speed(self):
+        self.__speed
+    
+    def accessor_range(self):
+        self.__range 
+
+    def accessor_status(self):
+        self.__status 
+    
+    def fire_bullet(self,b,sta):
+        self.__bullets -= 1 
+        if self.__bullets == 0:
+            self.__status == "Inactivate"
+
+    def get_name(self):
+        return self.__name
+    
+    def get_bullets(self):
+        return self.__bullets
+    
+    def get_speed(self):
+        return self.__speed
+    
+    def get_range(self):
+        return self.__range
+    
+    def get_status(self):
+        return self.__status 
+
+
 
 
 
